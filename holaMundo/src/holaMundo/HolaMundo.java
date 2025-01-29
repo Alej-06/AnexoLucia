e9d1464c21da2f30830c12cfe3a291bd57cf527d
Olga <olgarciaq11@gmail.com>
2025-01-29 13:05:57 +0100

    He añadido un comentario
    Signed-off-by: Olga <olgarciaq11@gmail.com>

---
+++ holaMundo/src/holaMundo/HolaMundo.java
@@ -0,0 +1,11 @@
+package holaMundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		
+		//Esto es para imprimir una línea de "Hola Mundo"
+		System.out.println("Hola mundo");
+
+	}
+}
